6b28a9ae6dd7fe26fae3d17cb6578f83a66ab458
Bill Anderson <steambadger@gmail.com>
2012-10-08 14:04:17 -0400

    Initial commit for Zombie Twitter rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydracamp::Application.config.secret_token = '0d436c169f0d4eb467dace0c0d1501b7d6381144016208df457d86cb3a7606527ea4e1609922b3bcea8ee119a948e0afa6d7b4145bd9018249254d5bcd4952aa'
